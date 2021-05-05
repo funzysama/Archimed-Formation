9e7232524fb2389c47f85b7a22cf062a9e622ea2
Monkeyaro <franck.daniel2015@orange.fr>
2021-05-05 21:28:02 +0200

    Ajout de controller

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UtilisateurRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/admin", name="admin_users")
      */
-    public function index(): Response
+    public function listerUtilisateurs(UtilisateurRepository $utilisateurRepository): Response
     {
+        $users = $utilisateurRepository->findAll();
+
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'controller_name'   => 'AdminController',
+            'users'             => $users
         ]);
     }
 }
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\Utilisateur;
+use App\Repository\UtilisateurRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main", name="main")
+     * @Route("/", name="main_home")
      */
-    public function index(): Response
+    public function index(UserInterface $user): Response
     {
+        dump($user);
+
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
         ]);
